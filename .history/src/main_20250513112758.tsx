9fa31c9b38c0efa351574c403a311848e33064b0
magicdeveloper250 <impanodeveloper@proton.me>
2025-08-29 13:28:54 +0200

    hosting

--- .history/src/main_20250513112758.tsx
+++ .history/src/main_20250513112758.tsx
@@ -5,9 +5,9 @@ import { ThemeProvider } from "./components/theme-provider";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  // <React.StrictMode>
+  <React.StrictMode>
     <ThemeProvider>
       <App />
     </ThemeProvider>
-  // </React.StrictMode>
+  </React.StrictMode>
 );
