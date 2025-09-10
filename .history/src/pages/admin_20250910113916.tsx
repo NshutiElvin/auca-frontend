22262b0a03c18823bd84ffba976693cdf6d3439f
magicdeveloper250 <impanodeveloper@proton.me>
2025-09-10 11:39:44 +0200

    changing slot logic

--- .history/src/pages/admin_20250910113916.tsx
+++ .history/src/pages/admin_20250910113916.tsx
@@ -179,7 +179,7 @@ export default function AdminMainPage() {
     <>
       <AppSidebar data={data} />
       <SidebarInset>
-        <header className="flex h-16 shrink-0 items-center justify-between gap-2 border-b px-4">
+        <header className="flex h-16 shrink-0 items-center gap-2 border-b px-4">
           <SidebarTrigger className="-ml-1" />
           <Separator orientation="vertical" className="mr-2 h-4" />
 
