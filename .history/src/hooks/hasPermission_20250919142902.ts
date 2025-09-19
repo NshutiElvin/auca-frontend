85a44318fe45b44d324e118b5a47bdf62435ea3c
magicdeveloper250 <impanodeveloper@proton.me>
2025-09-19 14:29:30 +0200

    getting real users from backend api

--- .history/src/hooks/hasPermission_20250919142902.ts
+++ .history/src/hooks/hasPermission_20250919142902.ts
@@ -3,7 +3,6 @@ import useAuth from './useAuth'
 export   function hasPermission(perm:string) {
 try {
       const {permissions}= useAuth()
-      console.log(perm)
       console.log( permissions.includes(perm))
   return  permissions.includes(perm)
   
