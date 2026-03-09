eac670dfbf2f5b8ca4e83268a2175ebb681d0e2c
magicdeveloper250 <impanodeveloper@proton.me>
2026-03-09 08:23:30 +0200

    Adding Search for searching for course in report page

--- src/pages/AttendanceReport.tsx
+++ src/pages/AttendanceReport.tsx
@@ -1203,7 +1203,7 @@ export function AttendanceReport() {
                 </h2>
               </div>
             </div>
-            // WITH THIS:
+            
             {courseAttendance && (
               <div className="grid grid-cols-2 sm:grid-cols-4 gap-2 mb-4">
                 {[
